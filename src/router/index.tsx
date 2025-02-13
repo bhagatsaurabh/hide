d9f7fa286387942a9a6c5db1089e83ca30a9f321
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2025-02-13 10:42:32 +0530

    scaffold

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,13 @@
 import { createBrowserRouter } from "react-router";
+import { Home } from "@/pages/Home/Home";
+import { Auth } from "@/pages/Auth/Auth";
+import { Features } from "@/pages/Features/Features";
+import { Dashboard } from "@/pages/Dashboard/Dashboard";
+import { Create } from "@/pages/Create/Create";
+import { Project } from "@/pages/Project/Project";
+import { User } from "@/pages/User/User";
+import { Environment } from "@/pages/Environment/Environment";
+import { CrashBoard } from "@/components/CrashBoard/CrashBoard";
 
 const router = createBrowserRouter([
   {
@@ -7,13 +16,40 @@ const router = createBrowserRouter([
     errorElement: <CrashBoard />,
   },
   {
-    path: "/flows",
-    element: <Flows />,
+    path: "/features",
+    element: <Features />,
     errorElement: <CrashBoard />,
   },
   {
-    path: "/flows/:id",
-    element: <Editor />,
+    path: "/auth",
+    element: <Auth />,
+    errorElement: <CrashBoard />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+    errorElement: <CrashBoard />,
+    children: [
+      {
+        path: "new",
+        element: <Create />,
+        errorElement: <CrashBoard />,
+      },
+      {
+        path: ":id",
+        element: <Project />,
+        errorElement: <CrashBoard />,
+      },
+    ],
+  },
+  {
+    path: "/profile",
+    element: <User />,
+    errorElement: <CrashBoard />,
+  },
+  {
+    path: "/env/:id",
+    element: <Environment />,
     errorElement: <CrashBoard />,
   },
 ]);
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 import SRI from "./plugins/subresource-integrity";
+import path from "path";
 
 export default defineConfig({
   plugins: [
@@ -34,4 +35,9 @@ export default defineConfig({
     }),
     SRI(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
