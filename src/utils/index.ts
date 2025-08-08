001fa52103ca78e00ff15e08c6e7b6b09d5c7839
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2025-08-08 18:59:59 +0200

    bugfixes

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -19,23 +19,9 @@ export const getPath = (node?: FNode) => {
   return path.reverse().join("/");
 };
 
-export const u8ToBase64 = (buf: Uint8Array) => {
-  let binary = "";
-  const len = buf.byteLength;
-  for (let i = 0; i < len; i++) {
-    binary += String.fromCharCode(buf[i]);
-  }
-  return btoa(binary);
-};
+export const u8ToBase64 = (buf: Uint8Array) => btoa(String.fromCharCode(...buf));
 
-export const base64ToU8 = (b64: string) => {
-  const str = atob(b64);
-  const bytes = new Uint8Array(str.length);
-  for (let i = 0; i < str.length; i++) {
-    bytes[i] = str.charCodeAt(i);
-  }
-  return bytes;
-};
+export const base64ToU8 = (b64: string) => Uint8Array.from(atob(b64), (c) => c.charCodeAt(0));
 
 export const debounce = <C extends (...args: never[]) => Promise<never> | Promise<void> | never>(
   func: C,
